ed1a4d771c835b98ba2dffb6cac6611b6b6f44ea
mhrifai <mkhrifai99@gmail.com>
2022-09-30 11:31:10 +0700

    menambahkan username

---
+++ MyBackendApp/DTO/AddUserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MyBackendApp.DTO
+{
+    public class AddUserDto
+    {
+        public  string Username { get; set; }
+        public string Password { get; set; }
+}
+}
--- MyBackendApp/Migrations/20220930041820_Add_ASPI.Designer.cs
+++ MyBackendApp/Migrations/20220930041820_Add_ASPI.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyBackendApp.DAL;
 
@@ -11,9 +12,10 @@ using MyBackendApp.DAL;
 namespace MyBackendApp.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220930041820_Add_ASPI")]
+    partial class Add_ASPI
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
